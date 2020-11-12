ae9a280d64c6904ee24d6ef751db199f12c2fd84
cydn95 <devstar.sky20@gmail.com>
2020-11-12 10:51:00 -0500

    remove unused commit

--- src/helper/contract.js
+++ src/helper/contract.js
@@ -7,6 +7,7 @@ VillainFactory 0xb3bbccac27cbf08ca4187ad6b840bdac5c9ef37d
 VillainStaking 0x335ed0F48715E6CF86D46D630dAdddcF7cBDDb6A
 HashToken 0x300496A0352e2E302a3d6df66e66B08625C305Be
 Old LPStaking 0x7ECE965a34fA67c9647B4030fF3BDf1160c6163C
+New LPStaking 0x0f5d42b4837e27cc7a7f3a0c59d982ff5598ce5d
 UniLP 0x65d0A154D2425CE2Fd5fED3BdaE94D9a9aFE55CE
 
 */
@@ -26,12 +27,12 @@ import PROD_UNISWAPV2PAIR_ABI from "./json/UniswapV2Pair_abi_prod.json";
 import DEV_NDR_ABI from "./json/NDR_abi_dev.json";
 import PROD_NDR_ABI from "./json/NDR_abi_prod.json";
 
-// LpStaking (new lp pool - mainnet is not ready)
+// LpStaking (new lp pool)
 export const DEV_LPSTAKING_ADDRESS = "0x1e94e09BaF1F9BB95e78DE116a7875e8870F9B0c";
 export const PROD_LPSTAKING_ADDRESS = "0x0f5d42b4837e27cc7a7f3a0c59d982ff5598ce5d";
 export { DEV_LPSTAKING_ABI, PROD_LPSTAKING_ABI };
 
-// LpStaking (old lp pool - mainnet is not ready)
+// LpStaking (old lp pool)
 export const DEV_LPSTAKING_OLD_ADDRESS = "0xb7709fe03262bad4ef1976ce3ffd927d9861d9c2";
 export const PROD_LPSTAKING_OLD_ADDRESS = "0x7ECE965a34fA67c9647B4030fF3BDf1160c6163C";
 export { DEV_LPSTAKING_OLD_ABI, PROD_LPSTAKING_OLD_ABI };
